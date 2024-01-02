b3fe0c9e27680f636642942ff71d2f803c90797b
StasyukVika <91117917+StasyukVika@users.noreply.github.com>
2024-01-02 21:37:36 +0200

    populate.sql

--- populate.sql
+++ populate.sql
@@ -1,39 +1,29 @@
-INSERT INTO Colonies (num_colonies, max_colonies, lost_colonies, percent_lost, added_colonies, renovated_colonies, percent_renovated)
+INSERT INTO Colonies (year, id_colonies)
 VALUES
-(7000, 7000, 1800, 26, 2800, 250, 4), 
-(35000, 35000, 4600, 13, 3400, 2100, 6),  
-(13000, 14000, 1500, 11, 1200, 90, 1),  
-(1440000, 1690000, 255000, 15, 250000, 124000, 7),  
-(3500, 12500, 1500, 12, 200, 140, 1),  
-(3900, 3900, 870, 22, 290, 0, 0), 
-(305000, 315000, 42000, 13, 54000, 25000, 8);
+('2015-01-23', 1), 
+('2015-02-12', 2),  
+('2015-02-27', 3),  
+('2016-03-08', 4),  
+('2016-03-15', 5),  
+('2016-03-23', 6), 
+('2017-04-18', 7);
 
-INSERT INTO State (state, state_code, num_colonies)
+INSERT INTO State (state, id_state)
 VALUES
-('Alabama', 'AL', 7000),
-('Arizona', 'AZ', 35000),
-('Arkansas', 'AR', 13000),
-('California', 'CA', 1440000),
-('Colorado', 'CO', 3500),
-('Connecticut', 'CT', 3900),
-('Florida', 'FL', 305000);
+('Alabama', 1),
+('Arizona', 2),
+('Arkansas', 3),
+('California', 4),
+('Colorado', 5),
+('Connecticut', 6),
+('Florida', 7);
 
-INSERT INTO Period (quarter, year, num_colonies)
+INSERT INTO Happening (condition, reason, con_year, id_state, id_colonies)
 VALUES
-(1, 2015, 7000),
-(2, 2015, 35000),
-(3, 2015, 13000),
-(4, 2015, 1440000),
-(5, 2015, 3500),
-(6, 2015, 3900),
-(7, 2015, 305000);
-
-INSERT INTO Causes_of_colonies_damage (varroa_mites, other_pests_and_parasites, diseases, pesticides, other, unknown, num_colonies)
-VALUES
-(10.0, 5.4, 0.0, 2.2, 9.1, 9.4, 7000),
-(26.9, 20.5, 0.1, 0.0, 1.8, 3.1, 35000),
-(17.6, 11.4, 1.5, 3.4, 1.0, 1.0, 13000),
-(24.7, 7.2, 3.0, 7.5, 6.5, 2.8, 1440000),
-(14.6, 0.9, 1.8, 0.6, 2.6, 5.9, 3500),
-(2.5, 1.4, 0.0, 0.0, 21.2, 2.4, 3900),
-(22.3, 13.5, 0.8, 8.9, 5.1, 4.4, 305000);
+('increased', 'weather', '2015-09-23', 1, 1),
+('decreased', 'pests', '2015-10-12', 2, 2),
+('decreased', 'diseases', '2015-10-27', 3, 3),
+('decreased', 'pesticides', '2016-11-08', 4, 4),
+('decreased', 'weather', '2016-11-15', 5, 5),
+('decreased', 'other', '2016-11-23', 6, 6),
+('increased', 'unknown', '2017-12-18', 7, 7);
