340aa40e4041826c0c13bfcba2432f99c616bebc
StasyukVika <91117917+StasyukVika@users.noreply.github.com>
2024-01-02 17:13:19 +0200

    create.sql

--- create.sql
+++ create.sql
@@ -1,42 +1,24 @@
-CREATE TABLE Colonies
-(
-  num_colonies VARCHAR(30) NOT NULL,
-  max_colonies VARCHAR(30) NOT NULL,
-  lost_colonies VARCHAR(30) NOT NULL,
-  percent_lost VARCHAR(30) NOT NULL,
-  added_colonies VARCHAR(30) NOT NULL,
-  renovated_colonies VARCHAR(30) NOT NULL,
-  percent_renovated VARCHAR(30) NOT NULL,
-  PRIMARY KEY (num_colonies)
-);
-
 CREATE TABLE State
 (
   state VARCHAR(30) NOT NULL,
-  state_code VARCHAR(30) NOT NULL,
-  num_colonies VARCHAR(30) NOT NULL,
-  PRIMARY KEY (state),
-  FOREIGN KEY (num_colonies) REFERENCES Colonies(num_colonies)
+  id_state INT NOT NULL,
+  PRIMARY KEY (id_state)
 );
 
-CREATE TABLE Period
+CREATE TABLE Colonies
 (
-  quarter VARCHAR(30) NOT NULL,
-  year VARCHAR(30) NOT NULL,
-  num_colonies VARCHAR(30) NOT NULL,
-  PRIMARY KEY (quarter),
-  FOREIGN KEY (num_colonies) REFERENCES Colonies(num_colonies)
+  year DATE NOT NULL,
+  id_colonies INT NOT NULL,
+  PRIMARY KEY (id_colonies)
 );
 
-CREATE TABLE Causes_of_colonies_damage
+CREATE TABLE Happening
 (
-  varroa_mites VARCHAR(30) NOT NULL,
-  other_pests_and_parasites VARCHAR(30) NOT NULL,
-  diseases VARCHAR(30) NOT NULL,
-  pesticides VARCHAR(30) NOT NULL,
-  other VARCHAR(30) NOT NULL,
-  unknown VARCHAR(30) NOT NULL,
-  num_colonies VARCHAR(30) NOT NULL,
-  PRIMARY KEY (varroa_mites),
-  FOREIGN KEY (num_colonies) REFERENCES Colonies(num_colonies)
+  condition VARCHAR(30) NOT NULL,
+  reason VARCHAR(30) NOT NULL,
+  con_year DATE NOT NULL,
+  id_state INT NOT NULL,
+  id_colonies INT NOT NULL,
+  FOREIGN KEY (id_state) REFERENCES State(id_state),
+  FOREIGN KEY (id_colonies) REFERENCES Colonies(id_colonies)
 );
