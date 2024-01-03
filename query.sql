a3fd9283b059e299ab9a5996a7704fb2ecba5be3
StasyukVika <91117917+StasyukVika@users.noreply.github.com>
2024-01-03 09:29:55 +0200

    query.sql

--- query.sql
+++ query.sql
@@ -3,14 +3,15 @@ SELECT COUNT(state) AS Colonies_from_Alabama
 FROM State
 WHERE state = 'Alabama';
 
--- Запит 2: Вивести максимальну кількість колоній медоносних бджіл з кожного штату
-SELECT State.state, Colonies.max_colonies
+-- Запит 2: Вивести назви штатів та стан колоній, де кількість бджіл змінилась через погодні умови
+SELECT State.state, Happening.condition
 FROM State
-INNER JOIN Colonies ON State.num_colonies=Colonies.num_colonies;
+INNER JOIN Happening ON State.id_state=Happening.id_state
+WHERE Happening.reason = 'weather';
 
-
--- Запит 3: Вивести колонії бджіл, де кількість втрат колоній менша за кількість доданих колоній
-SELECT State.state, Colonies.max_colonies
-FROM State
-INNER JOIN Colonies ON State.num_colonies=Colonies.num_colonies
-WHERE Colonies.lost_colonies < Colonies.added_colonies;
+-- Запит 2: Вивести назву і рік створення колонії, де кількість бджіл зменшилась
+SELECT State.state, Colonies.year
+FROM Happening
+INNER JOIN State ON State.id_state=Happening.id_state
+INNER JOIN Colonies ON Colonies.id_colonies=Happening.id_colonies
+WHERE Happening.condition = 'decreased';
