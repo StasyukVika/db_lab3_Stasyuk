0b858db759dae3dc59be725d8addd2dbc488a5b1
StasyukVika <91117917+StasyukVika@users.noreply.github.com>
2024-01-04 14:17:27 +0200

    populate.sql

--- populate.sql
+++ populate.sql
@@ -1,29 +1,29 @@
-INSERT INTO Colonies (year, id_colonies)
+INSERT INTO Colonies (id_colonies, year)
 VALUES
-('2015-01-23', 1), 
-('2015-02-12', 2),  
-('2015-02-27', 3),  
-('2016-03-08', 4),  
-('2016-03-15', 5),  
-('2016-03-23', 6), 
-('2017-04-18', 7);
+(1, '2015-01-23'), 
+(2, '2015-02-12'),  
+(3, '2015-02-27'),  
+(4, '2016-03-08'),  
+(5, '2016-03-15'),  
+(6, '2016-03-23'), 
+(7, '2017-04-18');
 
-INSERT INTO State (state, id_state)
+INSERT INTO State (id_state, state)
 VALUES
-('Alabama', 1),
-('Arizona', 2),
-('Arkansas', 3),
-('California', 4),
-('Colorado', 5),
-('Connecticut', 6),
-('Florida', 7);
+(1, 'Alabama'),
+(2, 'Arizona'),
+(3, 'Arkansas'),
+(4, 'California'),
+(5, 'Colorado'),
+(6, 'Connecticut'),
+(7, 'Florida');
 
-INSERT INTO Happening (condition, reason, con_year, id_state, id_colonies)
+INSERT INTO Happening (id_con, id_state, id_colonies, condition, reason, con_year)
 VALUES
-('increased', 'weather', '2015-09-23', 1, 1),
-('decreased', 'pests', '2015-10-12', 2, 2),
-('decreased', 'diseases', '2015-10-27', 3, 3),
-('decreased', 'pesticides', '2016-11-08', 4, 4),
-('decreased', 'weather', '2016-11-15', 5, 5),
-('decreased', 'other', '2016-11-23', 6, 6),
-('increased', 'unknown', '2017-12-18', 7, 7);
+( 1, 1, 1, 'increased', 'weather', '2015-09-23'),
+(2, 2, 2, 'decreased', 'pests', '2015-10-12'),
+(3, 3, 3, 'decreased', 'diseases', '2015-10-27'),
+(4, 4, 4, 'decreased', 'pesticides', '2016-11-08'),
+(5, 5, 5, 'decreased', 'weather', '2016-11-15'),
+(6, 6, 6, 'decreased', 'other', '2016-11-23'),
+(7, 7, 7, 'increased', 'unknown', '2017-12-18');
