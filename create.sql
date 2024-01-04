c9c5b4509b2f93e57390bcb6c4b6b785c6dad960
StasyukVika <91117917+StasyukVika@users.noreply.github.com>
2024-01-04 14:11:50 +0200

    create.sql

--- create.sql
+++ create.sql
@@ -1,24 +1,26 @@
 CREATE TABLE State
 (
-  state VARCHAR(30) NOT NULL,
   id_state INT NOT NULL,
+  state VARCHAR(30) NOT NULL,
   PRIMARY KEY (id_state)
 );
 
 CREATE TABLE Colonies
 (
-  year DATE NOT NULL,
   id_colonies INT NOT NULL,
+  year DATE NOT NULL,
   PRIMARY KEY (id_colonies)
 );
 
 CREATE TABLE Happening
 (
+  id_con INT NOT NULL,
+  id_state INT NOT NULL,
+  id_colonies INT NOT NULL,
   condition VARCHAR(30) NOT NULL,
   reason VARCHAR(30) NOT NULL,
   con_year DATE NOT NULL,
-  id_state INT NOT NULL,
-  id_colonies INT NOT NULL,
+  PRIMARY KEY (id_con),
   FOREIGN KEY (id_state) REFERENCES State(id_state),
   FOREIGN KEY (id_colonies) REFERENCES Colonies(id_colonies)
 );
