6f92db8f71861678162caeaf7d87d026ada55239
StasyukVika <91117917+StasyukVika@users.noreply.github.com>
2024-01-04 14:32:41 +0200

    query.sql

--- query.sql
+++ query.sql
@@ -1,7 +1,8 @@
--- Запит 1: Кількість бджолиних колоній з Алабами
-SELECT COUNT(state) AS Colonies_from_Alabama
-FROM State
-WHERE state = 'Alabama';
+-- Запит 1: Вивести назву штату, де знаходиться колонія бджіл, рік створення, стан колонії причину та рік змін в колонії
+SELECT State.state, Colonies.year, Happening.condition, Happening.reason, Happening.con_year
+FROM Happening
+INNER JOIN State ON State.id_state=Happening.id_state
+INNER JOIN Colonies ON Colonies.id_colonies=Happening.id_colonies;
 
 -- Запит 2: Вивести назви штатів та стан колоній, де кількість бджіл змінилась через погодні умови
 SELECT State.state, Happening.condition
